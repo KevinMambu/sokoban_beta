8f2c63b9403ac8e559eafd245a6fb1b67a1a69c3
kaegell <kev.mambu@gmail.com>
2014-04-11 21:25:23 +0200

    C'est bon, il ne reste plus que le dossier Code Blocks, j'ai un probleme avec la fonction jouer coup. Faut regler ça au plus vite

--- editeur.h
+++
@@ -1,9 +0,0 @@
-#ifndef _EDITEUR_
-#define _EDITEUR_
-
-void mur(plateau_t * p, int x, int y);
-void muraille_x(plateau_t * p, int x, int y, int longueur);
-void muraille_y(plateau_t * p, int x, int y, int longueur);
-void arrivee(plateau_t * p, int x, int y);
-
-#endif
